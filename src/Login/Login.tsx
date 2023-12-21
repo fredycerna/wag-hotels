171625efb6cb9679691a18fb1f9c18546548415f
fredycerna <fredycerna@gmail.com>
2023-12-21 11:54:06 -0600

    migrating styled components to css modules

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,37 +1,38 @@
 import React from 'react';
-import {
-  Container,
-  Header,
-  Logo,
-  LoginForm,
-  LoginTitle,
-  InputLabel,
-  Input,
-  LoginButton,
-  LinksContainer,
-  ForgotPasswordLink,
-  SignUpLink,
-} from './styles';
+import styles from './Login.module.css';
 
 const Login: () => React.JSX.Element = () => {
   return (
-    <Container>
-      <Header>
-        <Logo src='/images/logo.png' alt='Logo' />
-      </Header>
-      <LoginForm>
-        <LoginTitle>Login</LoginTitle>
-        <InputLabel htmlFor='email'>Email</InputLabel>
-        <Input type='email' id='email' />
-        <InputLabel htmlFor='password'>Password</InputLabel>
-        <Input type='password' id='password' />
-        <LoginButton type='submit'>Login</LoginButton>
-        <LinksContainer>
-          <ForgotPasswordLink href='#'>Forgot Password?</ForgotPasswordLink>
-          <SignUpLink href='#'>Sign Up</SignUpLink>
-        </LinksContainer>
-      </LoginForm>
-    </Container>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <img src='/images/logo.png' alt='Logo' className={styles.logo} />
+      </header>
+      <form className={styles.loginForm}>
+        <h2 className={styles.loginTitle}>Login</h2>
+        <label htmlFor='email' className={styles.inputLabel}>
+          Email
+        </label>
+        <input type='email' id='email' className={styles.input} />
+        <label htmlFor='password' className={styles.inputLabel}>
+          Password
+        </label>
+        <input type='password' id='password' className={styles.input} />
+        <button type='submit' className={styles.loginButton}>
+          Login
+        </button>
+        <div className={styles.linksContainer}>
+          <a
+            href='https://www.google.com'
+            className={styles.forgotPasswordLink}
+          >
+            Forgot Password?
+          </a>
+          <a href='https://www.google.com' className={styles.signUpLink}>
+            Sign Up
+          </a>
+        </div>
+      </form>
+    </div>
   );
 };
 
