417d356715c01e10f0b45cbe34745dbe6b24f3da
fredycerna <fredycerna@gmail.com>
2023-12-21 15:56:33 -0600

    adding api and constant file

---
+++ src/shared/api.ts
@@ -0,0 +1,35 @@
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
+import { API_BASE_URL, LOGIN_ENDPOINT } from './constants';
+import { AuthResponse, Credentials } from '../types/types';
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const login = async (
+  credentials: Credentials,
+): Promise<AuthResponse> => {
+  const config: AxiosRequestConfig = {
+    method: 'POST',
+  };
+
+  try {
+    const response: AxiosResponse<AuthResponse> = await api.post(
+      LOGIN_ENDPOINT,
+      credentials,
+      config,
+    );
+
+    return response.data;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      const axiosError = error as AxiosError;
+      throw axiosError.response?.data || axiosError.message;
+    } else {
+      throw error;
+    }
+  }
+};
---
+++ src/shared/constants.ts
@@ -0,0 +1,2 @@
+export const API_BASE_URL = 'https://testing.mywag.waghotels.com/api-mobile';
+export const LOGIN_ENDPOINT = '/api-token-auth';
